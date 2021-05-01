3ad203c28df40f4f3c9d4ba8be299332ad32d554
Aditya Parashar <adityaparashar863@gmail.com>
2021-05-01 18:00:56 +0530

    commit

--- book/urls.py
+++ book/urls.py
@@ -26,7 +26,7 @@ from django.conf.urls  import url
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('books.urls')),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
